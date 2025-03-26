c000953610d548b2b2ae06bb7b846c66e408dc27
JuniperDeSanto <juniperdesanto@gmail.com>
2025-03-26 13:37:36 -0400

    added checkExists endpoint
    Added checkExists endpoint to authController.js and authRoutes.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -78,3 +78,22 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
+
+export const checkExists = async (req, res) => 
+{
+  const { email } = req.body;
+
+  try 
+  {
+    const userExists = await User.findOne({ email });
+
+    if (userExists)
+      return res.status(400).json({ message: "Email already in use" });
+    else
+      return res.status(202).json({ message: "Email is unique" });         
+  }
+  catch (error)
+  {
+    res.status(500).json({ message: "Server error", error});
+  }
+};
