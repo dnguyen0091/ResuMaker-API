d61bae3f21762d4d56c97a5bbdd797a19e2012af
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-03-27 15:20:55 -0400

    Update aiRoutes.js

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -1,20 +1,20 @@
 import express from 'express';
-import multer from 'multer';
+//import multer from 'multer';
 import { analyzeResume } from '../controllers/analyzeController.js';
 import { generateEducationBullets, generateExperienceBullets, generateCustomBullets } from '../controllers/builderController.js';
 import path from 'path';
 
 // Multer storage setup
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads');  // Store files in 'uploads' directory
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);  // Keep the original file name
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads');  // Store files in 'uploads' directory
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);  // Keep the original file name
+//   }
+// });
 
-const upload = multer({ storage: storage });  // Apply disk storage configuration
+//const upload = multer({ storage: storage });  // Apply disk storage configuration
 
 const router = express.Router();
 
@@ -24,6 +24,6 @@ router.post('/generate-experience-bullets', generateExperienceBullets);
 router.post('/generate-custom-bullets', generateCustomBullets);
 
 // POST route for resume analysis
-router.post('/analyze', upload.single('file'), analyzeResume);  // Handle single file upload
-// router.post('/analyze',analyzeResume);
+//router.post('/analyze', upload.single('file'), analyzeResume);  // Handle single file upload
+router.post('/analyze',analyzeResume);
 export default router;
